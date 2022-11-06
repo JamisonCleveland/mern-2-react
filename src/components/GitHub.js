6888fd6b8a5a40e7824220af28b0bfa5042f8950
JamisonCleveland <jamisonc1128@gmail.com>
2022-11-05 20:13:46 -0600

    Linked to repository

--- src/components/GitHub.js
+++ src/components/GitHub.js
@@ -2,7 +2,7 @@
 const GitHub = () => {
     return (
         <p>
-            Here's my <a href="https://github.com/JamisonCleveland">GitHub repo</a>
+            Here's my <a href="https://github.com/JamisonCleveland/mern-2-react">GitHub repo</a>
         </p>
     )
 };
